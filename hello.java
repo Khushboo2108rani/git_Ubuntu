73f990b88ba5a62aaffb08a65ceb368c0c72a551
Khushboo2108rani <58814796+Khushboo2108rani@users.noreply.github.com>
2020-06-12 21:18:49 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
-public class hello{
-	public static void main(String[] args){
+public class hello {
+	public static void main(String[] args) {
 		for(int i=0;i<=10;i++)
 			{
 				System.out.println("Hello World" +" "+ i);
