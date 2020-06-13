2953c07fceaa7d1695abe72358f94e08ae85c89b
Khushboo2108rani <58814796+Khushboo2108rani@users.noreply.github.com>
2020-06-13 21:06:01 +0530

    Update jenkins_git.java

--- jenkins_git.java
+++ jenkins_git.java
@@ -2,7 +2,7 @@ public class jenkins_git {
 	public static void main(String[] args) {
 		for(int i=0;i<=10;i++)
 			{
-				System.out.println("integration  testing purpose" +" "+ i);
+				System.out.println("integration testing purpose" +" "+ i);
 			}
 	}
 }
