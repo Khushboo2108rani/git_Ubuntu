84701b384f068a9c18b234b7921bcc5ee4c4e16f
Khushboo2108rani <58814796+Khushboo2108rani@users.noreply.github.com>
2020-06-13 12:32:06 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello {
 	public static void main(String[] args) {
 		for(int i=0;i<=10;i++)
 			{
-				System.out.println("From GitHub and Jenkins integration" +" "+ i);
+				System.out.println("ABC From GitHub and Jenkins integration" +" "+ i);
 			}
 	}
 }
